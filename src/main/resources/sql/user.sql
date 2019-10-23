4ac925cb1e790ac695392dca5fdc109bfc4bea84
GitJavaProgramming <455547940@qq.com>
2019-10-23 13:59:13 +0800

    加上测试用的sql  创建表 插入数据

---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本机
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : springboot
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2019-10-23 13:56:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(32) NOT NULL,
+  `passWord` varchar(50) NOT NULL,
+  `realName` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', '123456', '111111');
+INSERT INTO `user` VALUES ('2', 'zhangp', 'nihao', 'pp');
