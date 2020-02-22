227f4f75c64643969bf481797313f655a57b49d1
GitJavaProgramming <455547940@qq.com>
2020-02-22 21:27:58 +0800

    开启rabbitMQ和kafka测试消息队列

--- src/main/java/org/pp/springboot_mybatis/SpringbootMybatisApplication.java
+++ src/main/java/org/pp/springboot_mybatis/SpringbootMybatisApplication.java
@@ -1,17 +1,13 @@
 package org.pp.springboot_mybatis;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.pp.spring.context.BeanTypeScanFilter;
-import org.pp.spring.context.IgnoreScanBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@ComponentScan(basePackages = {"org.pp"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {IgnoreScanBean.class}),
-        @ComponentScan.Filter(type = FilterType.CUSTOM, value = {BeanTypeScanFilter.class})})
+//@ComponentScan(basePackages = {"org.pp"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {IgnoreScanBean.class}),
+//        @ComponentScan.Filter(type = FilterType.CUSTOM, value = {BeanTypeScanFilter.class})})
 @EnableTransactionManagement // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @MapperScan("org.pp.springboot_mybatis.mapper") //扫描的mapper
 @SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
